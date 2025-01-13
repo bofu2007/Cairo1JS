c7c227a0c0504c29e6d6ba082b4c376fca43561b
bofu2007 <91390278+bofu2007@users.noreply.github.com>
2025-01-13 11:03:57 +0800

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,43 +1,32 @@
 "use server";
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import { Center } from '@chakra-ui/react';
-import { ChakraProvider } from '@chakra-ui/react';
-import ReactPlayer from 'react-player';
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Center } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import starknetjsImg from "../../public/Images/bofu.png";
-import { DisplayConnected } from './components/client/DisplayConnected';
-import LowerBanner from './components/client/LowerBanner';
+import { DisplayConnected } from "./components/client/DisplayConnected";
+import LowerBanner from "./components/client/LowerBanner";
+import MusicPlayer from "./components/MusicPlayer"; // Import MusicPlayer
 
 export default async function Page() {
-    return (
-        <ChakraProvider>
-            <div>
-                <p className={styles.bgText}>
-                    NFT music
-                </p>
-                <Center>
-                    <Image src={starknetjsImg} alt="starknet.js" width={150} />
-                </Center>
-                <p className={styles.bgText}>
-                    Please connect to Sepolia Testnet network
-                </p>
-                <div>
-                    <DisplayConnected />
-                </div>
-                <div style={{ marginTop: "2rem", textAlign: "center" }}>
-                    <h3>Enjoy Music</h3>
-                    {/* Music Player */}
-                    <ReactPlayer
-                        url="https://soundcloud.com/bofu-337905126/cheetah"
-                        controls
-                        width="100%"
-                        height="50px"
-                    />
-                </div>
-                <LowerBanner />
-            </div>
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider>
+      <div>
+        <p className={styles.bgText}>NFT music</p>
+        <Center>
+          <Image src={starknetjsImg} alt="starknet.js" width={150} />
+        </Center>
+        <p className={styles.bgText}>
+          Please connect to Sepolia Testnet network
+        </p>
+        <div>
+          <DisplayConnected />
+        </div>
+        <MusicPlayer /> {/* Use MusicPlayer here */}
+        <LowerBanner />
+      </div>
+    </ChakraProvider>
+  );
 }
