09032b79c11e60a1bbcaf4e6ed30c58d2a8265f2
bofu2007 <91390278+bofu2007@users.noreply.github.com>
2025-01-13 10:52:45 +0800

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,16 +1,16 @@
 "use server";
 
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 import { Center } from '@chakra-ui/react';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import ReactPlayer from 'react-player';
 
 import starknetjsImg from "../../public/Images/bofu.png";
 import { DisplayConnected } from './components/client/DisplayConnected';
 import LowerBanner from './components/client/LowerBanner';
 
 export default async function Page() {
-
     return (
         <ChakraProvider>
             <div>
@@ -18,19 +18,26 @@ export default async function Page() {
                     NFT music
                 </p>
                 <Center>
-                    <Image src={starknetjsImg} alt='starknet.js' width={150} />
+                    <Image src={starknetjsImg} alt="starknet.js" width={150} />
                 </Center>
                 <p className={styles.bgText}>
                     Please connect to Sepolia Testnet network
                 </p>
                 <div>
-                    <DisplayConnected></DisplayConnected>
-
+                    <DisplayConnected />
                 </div>
-                <LowerBanner></LowerBanner>
-            </div >
+                <div style={{ marginTop: "2rem", textAlign: "center" }}>
+                    <h3>Enjoy Music</h3>
+                    {/* Music Player */}
+                    <ReactPlayer
+                        url="https://soundcloud.com/bofu-337905126/cheetah"
+                        controls
+                        width="100%"
+                        height="50px"
+                    />
+                </div>
+                <LowerBanner />
+            </div>
         </ChakraProvider>
-    )
+    );
 }
-
-
